8be406567cbdca0dae009b142ae9cff126d310f6
NooberCong <inuyashathanhcongnguyen@gmail.com>
2021-06-17 23:15:48 +0700

    Allow anonymous hub access for QA

--- Hubs/QAHub.cs
+++ Hubs/QAHub.cs
@@ -1,13 +1,11 @@
 ﻿using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Hubs
 {
+    [AllowAnonymous]
     public class QAHub : Hub
     {
         public async Task sendQuestions(string content, string date)
